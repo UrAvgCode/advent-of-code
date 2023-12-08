0c557122b719f320ac382755492dd4dc9e7dd1f6
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-08 07:33:21 +0100

    2023 day 8

---
+++ 2023/day_8/haunted_wasteland_1.py
@@ -0,0 +1,26 @@
+# --- Day 8: Haunted Wasteland --- Part One ---
+
+if __name__ == '__main__':
+    with open("haunted_wasteland_input") as file:
+        instructions, lines = file.read().strip().split("\n\n")
+
+    move_map = {}
+    lines = lines.split("\n")
+    for line in lines:
+        parts = line.replace("= (", "").replace(",", "").replace(")", "").split()
+        move_map[parts[0]] = (parts[1], parts[2])
+
+    location = "AAA"
+    step_counter = 0
+    while True:
+        current_instruction = instructions[step_counter % len(instructions)]
+        if current_instruction == "L":
+            location = move_map[location][0]
+        else:
+            location = move_map[location][1]
+        step_counter += 1
+
+        if location == "ZZZ":
+            break
+
+    print("Part 1:", step_counter)
---
+++ 2023/day_8/haunted_wasteland_2.py
@@ -0,0 +1,34 @@
+# --- Day 8: Haunted Wasteland --- Part Two ---
+
+import math
+
+if __name__ == '__main__':
+    with open("haunted_wasteland_input") as file:
+        instructions, lines = file.read().strip().split("\n\n")
+
+    move_map = {}
+    for line in lines.replace("= (", "").replace(",", "").replace(")", "").split("\n"):
+        parts = line.split()
+        move_map[parts[0]] = (parts[1], parts[2])
+
+    locations = []
+    for key in move_map:
+        if key[-1] == "A":
+            locations.append(key)
+
+    step_counters = []
+    for i in range(len(locations)):
+        step_counter = 0
+        while True:
+            current_instruction = instructions[step_counter % len(instructions)]
+            if current_instruction == "L":
+                locations[i] = move_map[locations[i]][0]
+            else:
+                locations[i] = move_map[locations[i]][1]
+            step_counter += 1
+
+            if locations[i].endswith("Z"):
+                step_counters.append(step_counter)
+                break
+
+    print("Part 2:", math.lcm(*step_counters))
