f5be368aa37fcdc355db85220b041d7760b6702a
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-10 19:59:46 +0100

    update 2022 day 4

--- 2022/day_4/camp_cleanup.py
+++ 2022/day_4/camp_cleanup.py
@@ -1,22 +1,16 @@
 # --- Day 4: Camp Cleanup ---
 
 if __name__ == '__main__':
-    file = open("camp_cleanup_input", "r")
-    lines = file.readlines()
+    with open("camp_cleanup_input") as file:
+        pairs = file.read().strip().split("\n")
 
-    contains_other = 0
-    overlaps_other = 0
+    contains = 0
+    overlaps = 0
+    for line in pairs:
+        first, second = [tuple([int(bound) for bound in assignment.split("-")]) for assignment in line.split(",")]
+        overlap = (max(first[0], second[0]), min(first[1], second[1]))
+        contains += overlap == first or overlap == second
+        overlaps += overlap[0] <= overlap[1]
 
-    for line in lines:
-        ranges = line.strip().replace(",", "-").split("-")
-        asgmnt_one = set(range(int(ranges[0]), int(ranges[1]) + 1))
-        asgmnt_two = set(range(int(ranges[2]), int(ranges[3]) + 1))
-
-        if asgmnt_one.issubset(asgmnt_two) or asgmnt_two.issubset(asgmnt_one):
-            contains_other += 1
-
-        if asgmnt_one.intersection(asgmnt_two):
-            overlaps_other += 1
-
-    print("Part 1: " + str(contains_other))
-    print("Part 2: " + str(overlaps_other))
+    print("Part 1:", contains)
+    print("Part 2:", overlaps)
