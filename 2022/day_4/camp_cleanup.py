f0b2818217368dabfe0e9e39b13e392344a1031b
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-10 23:57:53 +0100

    update 2022 day 4

--- 2022/day_4/camp_cleanup.py
+++ 2022/day_4/camp_cleanup.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 
     contains = 0
     overlaps = 0
-    for line in pairs:
-        first, second = [tuple([int(bound) for bound in assignment.split("-")]) for assignment in line.split(",")]
+    for pair in pairs:
+        first, second = [tuple([int(bound) for bound in assignment.split("-")]) for assignment in pair.split(",")]
         overlap = (max(first[0], second[0]), min(first[1], second[1]))
         contains += overlap == first or overlap == second
         overlaps += overlap[0] <= overlap[1]
