0843b5f5300fc6b74d252b72e62f52fb53434ed3
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-10 18:32:36 +0100

    update 2022 day 2

--- 2022/day_2/rock_paper_scissors.py
+++ 2022/day_2/rock_paper_scissors.py
@@ -1,17 +1,14 @@
 # --- Day 2: Rock Paper Scissors ---
 
 if __name__ == '__main__':
-    file = open("rock_paper_scissors_input", "r")
-    lines = file.readlines()
+    with open("rock_paper_scissors_input") as file:
+        rounds = file.read().strip().split("\n")
 
-    first_tactic = {"A Y": 8, "B Z": 9, "C X": 7, "A X": 4, "B Y": 5, "C Z": 6, "A Z": 3, "B X": 1, "C Y": 2}
-    correct_tactic = {"A X": 3, "B X": 1, "C X": 2, "A Y": 4, "B Y": 5, "C Y": 6, "A Z": 8, "B Z": 9, "C Z": 7}
+    first_strategy = {"A Y": 8, "B Z": 9, "C X": 7, "A X": 4, "B Y": 5, "C Z": 6, "A Z": 3, "B X": 1, "C Y": 2}
+    correct_strategy = {"A X": 3, "B X": 1, "C X": 2, "A Y": 4, "B Y": 5, "C Y": 6, "A Z": 8, "B Z": 9, "C Z": 7}
 
-    score_one = 0
-    score_two = 0
-    for line in lines:
-        score_one += first_tactic[line.strip()]
-        score_two += correct_tactic[line.strip()]
+    score_one = sum(first_strategy[match] for match in rounds)
+    score_two = sum(correct_strategy[match] for match in rounds)
 
-    print("Part 1: " + str(score_one))
-    print("Part 2: " + str(score_two))
+    print("Part 1:", score_one)
+    print("Part 2:", score_two)
