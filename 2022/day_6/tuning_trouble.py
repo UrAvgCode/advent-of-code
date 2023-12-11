5de6be5b575002c1e9b7e317cea89f43aa9db8f8
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-12 00:29:59 +0100

    update 2022 day 6

--- 2022/day_6/tuning_trouble.py
+++ 2022/day_6/tuning_trouble.py
@@ -1,17 +1,14 @@
 # --- Day 6: Tuning Trouble ---
 
-def marker_at(signal, marker_length):
-    for i in range(len(signal)):
-        for j in range(marker_length - 1):
-            if signal[i + j] in signal[i + j + 1:i + marker_length]:
-                break
-            elif j == marker_length - 2:
-                return i + marker_length
+def find_marker(length):
+    for i in range(len(datastream)):
+        if len(set(datastream[i:i + length])) == length:
+            return i + length
 
 
 if __name__ == '__main__':
-    file = open("tuning_trouble_input", "r")
-    line = file.readlines()[0].strip()
+    with open("tuning_trouble_input") as file:
+        datastream = file.read()
 
-    print("Part 1: " + str(marker_at(line, 4)))
-    print("Part 2: " + str(marker_at(line, 14)))
+    print("Part 1:", find_marker(4))
+    print("Part 2:", find_marker(14))
