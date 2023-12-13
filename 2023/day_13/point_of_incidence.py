c6922c4c6ccc3f2e5604e70a06e98015602ff7d3
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-13 09:11:18 +0100

    merge 2023 day 13

--- 2023/day_13/point_of_incidence.py
+++ 2023/day_13/point_of_incidence.py
@@ -2,10 +2,8 @@
 
 from collections import deque
 
-if __name__ == '__main__':
-    with open("point_of_incidence_input") as file:
-        patterns = [p.splitlines() for p in file.read().split("\n\n")]
 
+def get_reflection_sum(accuracy=0):
     reflection_sum = 0
     for pattern in patterns:
         transpose = list(zip(*pattern))
@@ -21,12 +19,19 @@ if __name__ == '__main__':
                         for char, char2 in zip(row, row_que.pop()):
                             if char != char2:
                                 difference += 1
-                        if difference > 1:
+                        if difference > accuracy:
                             break
                 else:
                     if mult:
                         equal *= 100
-                    if difference == 1:
+                    if difference == accuracy:
                         reflection_sum += equal
+    return reflection_sum
 
-    print("Part 2:", reflection_sum)
+
+if __name__ == '__main__':
+    with open("point_of_incidence_input") as file:
+        patterns = [p.splitlines() for p in file.read().split("\n\n")]
+
+    print("Part 1:", get_reflection_sum())
+    print("Part 2:", get_reflection_sum(1))
