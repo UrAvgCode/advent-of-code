fc6e82525a398c6cd39ea1dd62d3b443b26951ff
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-13 22:22:27 +0100

    merge 2023 day 6

--- 2023/day_6/wait_for_it.py
+++ 2023/day_6/wait_for_it.py
@@ -7,22 +7,22 @@ def get_number_of_ways(time, distance):
     root = math.sqrt(time ** 2 - 4 * (distance + 1))
     start = (time - root) / 2
     end = (time + root) / 2
-    return math.floor(end) - math.ceil(start) + 1
+    return math.floor(end) - math.floor(start)
 
 
 if __name__ == '__main__':
     with open("wait_for_it_input") as file:
-        times, distances = file.read().strip().split("\n")
+        times, distances = file.read().splitlines()
 
-    times_part1 = [int(time) for time in times.split()[1:]]
-    distances_part1 = [int(distance) for distance in distances.split()[1:]]
+    times_part_one = [int(time) for time in times.split()[1:]]
+    distances_part_one = [int(distance) for distance in distances.split()[1:]]
 
-    time_part2 = int(times.split(" ", 1)[1].replace(" ", ""))
-    distance_part2 = int(distances.split(" ", 1)[1].replace(" ", ""))
+    time_part_two = int(times.split(" ", 1)[1].replace(" ", ""))
+    distance_part_two = int(distances.split(" ", 1)[1].replace(" ", ""))
 
     ways_multiplied = 1
-    for time, distance in zip(times_part1, distances_part1):
+    for time, distance in zip(times_part_one, distances_part_one):
         ways_multiplied *= get_number_of_ways(time, distance)
 
     print("Part 1:", ways_multiplied)
-    print("Part 2:", get_number_of_ways(time_part2, distance_part2))
+    print("Part 2:", get_number_of_ways(time_part_two, distance_part_two))
