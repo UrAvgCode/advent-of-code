c7b00476654fb24f60faff5b307eac998f0364a3
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-13 12:00:42 +0100

    update 2023 day 1

--- 2023/day_1/trebuchet.py
+++ 2023/day_1/trebuchet.py
@@ -1,38 +1,33 @@
 # --- Day 1: Trebuchet?! ---
 
-if __name__ == '__main__':
-    file = open("trebuchet_input", "r")
-    lines = file.readlines()
-
-    calibration_values_one = 0
-    calibration_values_two = 0
-
-    numberStrings = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-
+def get_calibration_values(digit_words):
+    calibration_values = 0
     for line in lines:
-        firstDigit_one = 0
-        secondDigit_one = 0
+        first_digit = last_digit = 0
 
-        firstDigit_two = 0
-        secondDigit_two = 0
+        for i, char in enumerate(line):
+            if char.isdigit():
+                last_digit = int(char)
+                if first_digit == 0:
+                    first_digit = last_digit
+            elif digit_words:
+                for digit_word, digit_value in digit_words:
+                    if line[i:i + len(digit_word)] == digit_word:
+                        last_digit = digit_value
+                        if first_digit == 0:
+                            first_digit = digit_value
+                        break
 
-        for i in range(len(line)):
-            for number in numberStrings:
-                if line[i:i + len(number)] == number:
-                    if firstDigit_two == 0:
-                        firstDigit_two = numberStrings.index(number) + 1
-                    secondDigit_two = numberStrings.index(number) + 1
+        calibration_values += first_digit * 10 + last_digit
 
-            if line[i].isdigit():
-                if firstDigit_one == 0:
-                    firstDigit_one = int(line[i])
-                if firstDigit_two == 0:
-                    firstDigit_two = int(line[i])
-                secondDigit_one = int(line[i])
-                secondDigit_two = int(line[i])
+    return calibration_values
 
-        calibration_values_one += firstDigit_one * 10 + secondDigit_one
-        calibration_values_two += firstDigit_two * 10 + secondDigit_two
 
-    print("Part 1: " + str(calibration_values_one))
-    print("Part 2: " + str(calibration_values_two))
+if __name__ == '__main__':
+    with open("trebuchet_input") as file:
+        lines = file.read().splitlines()
+
+    digit_words = [(item, i) for i, item in enumerate("one two three four five six seven eight nine".split(), 1)]
+
+    print("Part 1:", get_calibration_values([]))
+    print("Part 2:", get_calibration_values(digit_words))
