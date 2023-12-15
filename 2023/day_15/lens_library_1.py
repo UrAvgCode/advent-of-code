ccb3b9fa7f7414c01122e4f5370e489ba01338d7
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-15 07:30:50 +0100

    2023 day 15 part 1

---
+++ 2023/day_15/lens_library_1.py
@@ -0,0 +1,13 @@
+# --- Day 15: Lens Library --- Part One ---
+
+def get_hash(string):
+    current_value = 0
+    for char in string:
+        current_value = ((current_value + ord(char)) * 17) % 256
+    return current_value
+
+
+if __name__ == '__main__':
+    with open("lens_library_input") as file:
+        lines = file.read().strip().split(',')
+    print("Part 1:", sum(get_hash(line) for line in lines))
