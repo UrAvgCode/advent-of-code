f7c2efd1a71985f7e27cb3dfad525cf1bc801707
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-16 12:41:24 +0100

    update 2023 day 16

--- 2023/day_16/the_floor_will_be_lava.py
+++ 2023/day_16/the_floor_will_be_lava.py
@@ -2,41 +2,31 @@
 
 from collections import deque
 
+directions = {'E': (1, 0), 'W': (-1, 0), 'N': (0, -1), 'S': (0, 1)}
+right_mirror = {'E': 'N', 'W': 'S', 'N': 'E', 'S': 'W'}
+left_mirror = {'E': 'S', 'W': 'N', 'N': 'W', 'S': 'E'}
 
-def get_energized_tiles(start_beam=(-1, 0, 'E')):
+
+def get_energized_tiles(grid, start_beam=(-1, 0, 'E')):
     beam_queue = deque([start_beam])
     energized_tiles = set()
     beam_history = set()
 
     while beam_queue:
-        beam = beam_queue.pop()
-        if beam in beam_history:
+        x, y, direction = beam_queue.pop()
+        if (x, y, direction) in beam_history:
             continue
 
-        x, y, direction = beam
         energized_tiles.add((x, y))
         beam_history.add((x, y, direction))
 
-        match direction:
-            case 'E':
-                next_x = x + 1
-                next_y = y
-            case 'W':
-                next_x = x - 1
-                next_y = y
-            case 'N':
-                next_x = x
-                next_y = y - 1
-            case 'S':
-                next_x = x
-                next_y = y + 1
-            case _:
-                next_x = next_y = -1
+        dx, dy = directions[direction]
+        next_x, next_y = x + dx, y + dy
 
-        if next_x < 0 or next_y < 0 or next_y >= len(lines) or next_x >= len(lines[next_y]):
+        if next_x < 0 or next_y < 0 or next_y >= len(grid) or next_x >= len(grid[next_y]):
             continue
 
-        match lines[next_y][next_x]:
+        match grid[next_y][next_x]:
             case '.':
                 beam_queue.append((next_x, next_y, direction))
             case '|':
@@ -52,46 +42,22 @@ def get_energized_tiles(start_beam=(-1, 0, 'E')):
                     beam_queue.append((next_x, next_y, 'E'))
                     beam_queue.append((next_x, next_y, 'W'))
             case '/':
-                match direction:
-                    case 'E':
-                        new_dir = 'N'
-                    case 'W':
-                        new_dir = 'S'
-                    case 'N':
-                        new_dir = 'E'
-                    case 'S':
-                        new_dir = 'W'
-                    case _:
-                        new_dir = None
-                beam_queue.append((next_x, next_y, new_dir))
+                beam_queue.append((next_x, next_y, right_mirror[direction]))
             case '\\':
-                match direction:
-                    case 'E':
-                        new_dir = 'S'
-                    case 'W':
-                        new_dir = 'N'
-                    case 'N':
-                        new_dir = 'W'
-                    case 'S':
-                        new_dir = 'E'
-                    case _:
-                        new_dir = None
-                beam_queue.append((next_x, next_y, new_dir))
+                beam_queue.append((next_x, next_y, left_mirror[direction]))
     return len(energized_tiles) - 1
 
 
 if __name__ == '__main__':
     with open("the_floor_will_be_lava_input") as file:
-        lines = [list(line) for line in file.read().splitlines()]
+        grid = file.read().splitlines()
 
     energized_list = []
-    for y in range(len(lines)):
-        energized_list.append(get_energized_tiles((-1, y, 'E')))
-        energized_list.append(get_energized_tiles((len(lines[0]), y, 'W')))
+    for i in range(len(grid)):
+        energized_list.append(get_energized_tiles(grid, (i, -1, 'S')))
+        energized_list.append(get_energized_tiles(grid, (i, len(grid), 'N')))
+        energized_list.append(get_energized_tiles(grid, (-1, i, 'E')))
+        energized_list.append(get_energized_tiles(grid, (len(grid[i]), i, 'W')))
 
-    for x in range(len(lines[0])):
-        energized_list.append(get_energized_tiles((x, -1, 'S')))
-        energized_list.append(get_energized_tiles((x, len(lines), 'N')))
-
-    print("Part 1:", get_energized_tiles())
+    print("Part 1:", get_energized_tiles(grid))
     print("Part 2:", max(energized_list))
