4099d2a18e556eeb0527dd60fb4eb9b12c6f8c88
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-21 17:27:06 +0100

    update 2023 day 3

--- 2023/day_03/gear_ratios.py
+++ 2023/day_03/gear_ratios.py
@@ -1,46 +1,46 @@
 # --- Day 3: Gear Ratios ---
 
-if __name__ == '__main__':
-    file = open("gear_ratios_input", "r")
-    lines = file.readlines()
+if __name__ == "__main__":
+    with open("gear_ratios_input") as file:
+        schematic = ["." + line + "." for line in file.read().splitlines()]
 
-    symbols = "*-+/=%#&@!$?^~"
+    schematic.append("." * len(schematic[0]))
+    schematic.insert(0, "." * len(schematic[0]))
 
-    sum_part_numbers = 0
+    symbols = set("*-+/=%#&@$")
+
+    part_numbers_sum = 0
     gear_ratios = {}
-
-    for i in range(len(lines)):
-        gear_at = ""
+    for y in range(1, len(schematic) - 1):
+        gear_at = ()
         number = ""
         is_number = False
-        for j in range(len(lines[i])):
-            if lines[i][j].isdigit():
-                number += lines[i][j]
+        for x in range(len(schematic[y])):
+            if schematic[y][x].isdigit():
+                number += schematic[y][x]
 
-                for k in range(-1, 2, 1):
-                    for n in range(-1, 2, 1):
-                        if i + k < 0 or i + k >= len(lines) or j + n < 0 or j + n >= len(lines[i]):
-                            continue
-                        if lines[i + k][j + n] in symbols:
+                for dy in (-1, 0, 1):
+                    for dx in (-1, 0, 1):
+                        if schematic[y + dy][x + dx] in symbols:
                             is_number = True
-                            if lines[i + k][j + n] == "*":
-                                gear_at = str(i + k) + "," + str(j + n)
+                            if schematic[y + dy][x + dx] == "*":
+                                gear_at = (y + dy, x + dx)
             else:
                 if is_number:
-                    sum_part_numbers += int(number)
+                    part_numbers_sum += int(number)
                     is_number = False
 
                     if len(gear_at) > 0:
                         if gear_at not in gear_ratios:
                             gear_ratios[gear_at] = []
                         gear_ratios[gear_at].append(int(number))
-                        gear_at = ""
+                        gear_at = ()
                 number = ""
 
     gear_score = 0
-    for key in gear_ratios:
-        if len(gear_ratios[key]) == 2:
-            gear_score += gear_ratios[key][0] * gear_ratios[key][1]
+    for part_numbers in gear_ratios.values():
+        if len(part_numbers) == 2:
+            gear_score += part_numbers[0] * part_numbers[1]
 
-    print("Part 1: " + str(sum_part_numbers))
-    print("Part 2: " + str(gear_score))
+    print("Part 1:", part_numbers_sum)
+    print("Part 2:", gear_score)
