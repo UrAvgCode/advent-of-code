d54840bc2bc6e6a39f8998fff4d87db8b81a362c
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2023-12-25 14:42:44 +0100

    update 2023 day 25 part 1

--- 2023/day_25/snowverload_1.py
+++ 2023/day_25/snowverload_1.py
@@ -1,14 +1,16 @@
 # --- Day 25: Snowverload --- Part One ---
 
 import networkx as nx
-import matplotlib.pyplot as plt
+from time import perf_counter
 
 if __name__ == '__main__':
     with open('snowverload_input') as file:
         lines = file.read().splitlines()
 
-    components = set()
+    start_time = perf_counter()
+
     edges = set()
+    components = set()
     for line in lines:
         this, others = line.split(': ')
         others = others.split()
@@ -17,35 +19,32 @@ if __name__ == '__main__':
         for other in others:
             edges.add((this, other))
 
-    G = nx.Graph()
-    G.add_nodes_from(components)
-    G.add_edges_from(edges)
+    Graph = nx.Graph()
+    Graph.add_nodes_from(components)
+    Graph.add_edges_from(edges)
+    removed_edges = nx.minimum_edge_cut(Graph)
 
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_size=700, node_color="skyblue", font_size=15, font_color="black",
-            font_weight="bold", edge_color="gray", linewidths=1, alpha=0.7)
-    plt.style.use('ggplot')
-    plt.show()
+    for edge in removed_edges:
+        if edge in edges:
+            edges.remove(edge)
+        else:
+            edges.remove((edge[1], edge[0]))
 
-    # manual input
-    edges.remove(('qnd', 'mbk'))
-    edges.remove(('rrl', 'pcs'))
-    edges.remove(('ddl', 'lcm'))
-
-    first_group = set()
+    group = set()
     while True:
         for edge in edges:
-            if not first_group:
-                first_group.add(edge[0])
-                first_group.add(edge[1])
+            if not group:
+                group.add(edge[0])
+                group.add(edge[1])
                 edges.remove(edge)
                 break
-            elif edge[0] in first_group or edge[1] in first_group:
-                first_group.add(edge[0])
-                first_group.add(edge[1])
+            elif edge[0] in group or edge[1] in group:
+                group.add(edge[0])
+                group.add(edge[1])
                 edges.remove(edge)
                 break
         else:
             break
 
-    print(len(first_group) * len(components - first_group))
+    print('Part 2:', len(group) * len(components - group))
+    print('Time:', perf_counter() - start_time)
