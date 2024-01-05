c6c6295e792d564e54a3070f10d8e284db2a2205
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-05 22:56:35 +0100

    2021 day 6 part 1

--- 2021/day_06/lanternfish_1.py
+++ 2021/day_06/lanternfish_1.py
@@ -1,15 +1,15 @@
-# --- Day 6: Lanternfish ---
+# --- Day 6: Lanternfish --- Part One ---
 
 if __name__ == '__main__':
     with open('lanternfish_input') as file:
         fish_list = list(map(int, file.read().split(',')))
 
     for _ in range(80):
-        for i, fish in enumerate(fish_list):
+        for i, fish in enumerate(fish_list.copy()):
             if fish > 0:
                 fish_list[i] -= 1
             else:
                 fish_list[i] = 6
-                fish_list.append(9)
+                fish_list.append(8)
 
     print('Part 1:', len(fish_list))
