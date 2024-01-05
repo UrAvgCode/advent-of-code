21e8c411d0231bd3c446628a77e3d6d3ed5f8e66
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-05 16:49:55 +0100

    merge 2021 day 5

---
+++ 2021/day_05/hydrothermal_venture.py
@@ -0,0 +1,33 @@
+# --- Day 5: Hydrothermal Venture ---
+
+def sign(x):
+    return (x > 0) - (x < 0)
+
+
+def calculate_overlaps(vents, diagonals=False):
+    diagram = {}
+    for vent in vents:
+        (x1, y1), (x2, y2) = vent
+        if diagonals or x1 == x2 or y1 == y2:
+            while x1 != x2 or y1 != y2:
+                diagram[(x1, y1)] = diagram.get((x1, y1), 0) + 1
+                x1 += sign(x2 - x1)
+                y1 += sign(y2 - y1)
+            diagram[(x2, y2)] = diagram.get((x2, y2), 0) + 1
+
+    return sum(1 for value in diagram.values() if value > 1)
+
+
+if __name__ == '__main__':
+    with open('hydrothermal_venture_input') as file:
+        lines = file.read().splitlines()
+
+    vents = []
+    for line in lines:
+        start, end = line.split(' -> ')
+        start = tuple(map(int, start.split(',')))
+        end = tuple(map(int, end.split(',')))
+        vents.append((start, end))
+
+    print('Part 1:', calculate_overlaps(vents))
+    print('Part 2:', calculate_overlaps(vents, True))
