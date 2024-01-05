00978d3fd2a6fb26eafe72a3a1b0a54f1af45b27
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-05 19:56:51 +0100

    update 2022 day 11

--- 2022/day_11/monkey_in_the_middle_1.py
+++ 2022/day_11/monkey_in_the_middle_1.py
@@ -1,63 +1,42 @@
-# --- Day 11: Monkey in the Middle ---
-
-class Monkey:
-    def __init__(self, items, operation, test_number, true_monkey, false_monkey):
-        self.items = items
-        self.operation = operation
-        self.test_number = test_number
-        self.true_monkey = true_monkey
-        self.false_monkey = false_monkey
-        self.inspected_items = 0
-
-    def pop(self, index):
-        return self.items.pop(index)
-
-    def append(self, item):
-        self.items.append(item)
-
-    def test(self, number):
-        return number % self.test_number == 0
-
-    def calculate_worry_level(self, item):
-        if self.operation == ["*", "old"]:
-            return item * item
-        elif self.operation[0] == "+":
-            return item + int(self.operation[1])
-        elif self.operation[0] == "*":
-            return item * int(self.operation[1])
-
+# --- Day 11: Monkey in the Middle --- Part One ---
 
 if __name__ == '__main__':
-    file = open("monkey_in_the_middle_input", "r")
-    lines = file.readlines()
+    with open('monkey_in_the_middle_input') as file:
+        monkey_strings = file.read().split('\n\n')
 
     monkeys = []
-    for i in range(8):
-        items_parts = lines[i * 7 + 1].replace(",", "").split()[2:]
-        items = []
-        for item in items_parts:
-            items.append(int(item))
-        operation = lines[i * 7 + 2].split()[4:]
-        test = int(lines[i * 7 + 3].split()[-1])
-        true_monkey = int(lines[i * 7 + 4].split()[-1])
-        false_monkey = int(lines[i * 7 + 5].split()[-1])
+    for i, monkey in enumerate(monkey_strings):
+        m, items, operation, test, true_monkey, false_monkey = monkey.splitlines()
+        items = list(map(int, items.replace(',', '').split()[2:]))
+        operation = operation.split()
+        operation = (operation[4], operation[5])
+        test = int(test.split()[-1])
+        true_monkey = int(true_monkey.split()[-1])
+        false_monkey = int(false_monkey.split()[-1])
+        monkeys.append([items, operation, test, true_monkey, false_monkey, 0])
 
-        monkeys.append(Monkey(items, operation, test, true_monkey, false_monkey))
-
-    for i in range(20):
+    inspected_items = 0
+    for _ in range(20):
         for monkey in monkeys:
-            for j in range(len(monkey.items)):
-                new_item = monkey.calculate_worry_level(monkey.pop(0))
+            items, operation, test, true_monkey, false_monkey, inspected = monkey
+            while items:
+                new_item = items.pop()
+                operator, operand = operation
+                if operand == 'old':
+                    new_item = new_item ** 2
+                elif operator == '+':
+                    new_item += int(operand)
+                elif operator == '*':
+                    new_item *= int(operand)
                 new_item //= 3
-                monkey.inspected_items += 1
-                if monkey.test(new_item):
-                    monkeys[monkey.true_monkey].append(new_item)
-                else:
-                    monkeys[monkey.false_monkey].append(new_item)
 
-    monkey_active_counts = []
-    for monkey in monkeys:
-        monkey_active_counts.append(monkey.inspected_items)
+                monkey[-1] += 1
+                if new_item % test == 0:
+                    monkeys[true_monkey][0].append(new_item)
+                else:
+                    monkeys[false_monkey][0].append(new_item)
+
+    monkey_active_counts = [monkey[-1] for monkey in monkeys]
     monkey_active_counts.sort(reverse=True)
 
-    print("Part 1: " + str(monkey_active_counts[0] * monkey_active_counts[1]))
+    print('Part 1:', monkey_active_counts[0] * monkey_active_counts[1])
--- 2022/day_11/monkey_in_the_middle_2.py
+++ 2022/day_11/monkey_in_the_middle_2.py
@@ -5,85 +5,63 @@ prime_numbers = (2, 3, 5, 7, 11, 13, 17, 19, 23)
 
 class Item:
     def __init__(self, value):
-        self.modulos = []
+        self.modulos = {prime: value % prime for prime in prime_numbers}
+
+    def __add__(self, other):
         for prime in prime_numbers:
-            self.modulos.append(value % prime)
+            self.modulos[prime] = (self.modulos[prime] + other) % prime
+        return self
 
-    def add(self, value):
-        for prime in range(len(self.modulos)):
-            self.modulos[prime] = (self.modulos[prime] + value) % prime_numbers[prime]
+    def __mul__(self, other):
+        for prime in prime_numbers:
+            self.modulos[prime] = (self.modulos[prime] * other) % prime
+        return self
 
-    def multiply(self, value):
-        for prime in range(len(self.modulos)):
-            self.modulos[prime] = (self.modulos[prime] * value) % prime_numbers[prime]
+    def __mod__(self, other):
+        return self.modulos[other]
 
-    def square(self):
-        for prime in range(len(self.modulos)):
-            self.modulos[prime] = (self.modulos[prime] * self.modulos[prime]) % prime_numbers[prime]
-
-    def test(self, value):
-        return self.modulos[prime_numbers.index(value)] == 0
-
-
-class Monkey:
-    def __init__(self, items, operation, test_number, true_monkey, false_monkey):
-        self.items = items
-        self.operation = operation
-        self.test_number = test_number
-        self.true_monkey = true_monkey
-        self.false_monkey = false_monkey
-        self.inspected_items = 0
-
-    def pop(self, index):
-        return self.items.pop(index)
-
-    def append(self, item):
-        self.items.append(item)
-
-    def test(self, item):
-        return item.test(self.test_number)
-
-    def calculate_worry_level(self, item):
-        if self.operation == ["*", "old"]:
-            item.square()
-        elif self.operation[0] == "+":
-            item.add(int(self.operation[1]))
-        elif self.operation[0] == "*":
-            item.multiply(int(self.operation[1]))
+    def __pow__(self, other):
+        for prime in prime_numbers:
+            self.modulos[prime] = pow(self.modulos[prime], other) % prime
+        return self
 
 
 if __name__ == '__main__':
-    file = open("monkey_in_the_middle_input", "r")
-    lines = file.readlines()
+    with open('monkey_in_the_middle_input') as file:
+        monkey_strings = file.read().split('\n\n')
 
     monkeys = []
-    for i in range(8):
-        items_parts = lines[i * 7 + 1].replace(",", "").split()[2:]
-        items = []
-        for item in items_parts:
-            items.append(Item(int(item)))
-        operation = lines[i * 7 + 2].split()[4:]
-        test = int(lines[i * 7 + 3].split()[-1])
-        true_monkey = int(lines[i * 7 + 4].split()[-1])
-        false_monkey = int(lines[i * 7 + 5].split()[-1])
+    for i, monkey in enumerate(monkey_strings):
+        m, items, operation, test, true_monkey, false_monkey = monkey.splitlines()
+        items = list(map(int, items.replace(',', '').split()[2:]))
+        operation = operation.split()
+        operation = (operation[4], operation[5])
+        test = int(test.split()[-1])
+        true_monkey = int(true_monkey.split()[-1])
+        false_monkey = int(false_monkey.split()[-1])
+        monkeys.append([list(map(Item, items)), operation, test, true_monkey, false_monkey, 0])
 
-        monkeys.append(Monkey(items, operation, test, true_monkey, false_monkey))
-
-    for i in range(10000):
+    inspected_items = 0
+    for _ in range(10000):
         for monkey in monkeys:
-            for j in range(len(monkey.items)):
-                new_item = monkey.pop(0)
-                monkey.calculate_worry_level(new_item)
+            items, operation, test, true_monkey, false_monkey, inspected = monkey
+            while items:
+                new_item = items.pop()
+                operator, operand = operation
+                if operand == 'old':
+                    new_item = new_item ** 2
+                elif operator == '+':
+                    new_item += int(operand)
+                elif operator == '*':
+                    new_item *= int(operand)
 
-                monkey.inspected_items += 1
-                if monkey.test(new_item):
-                    monkeys[monkey.true_monkey].append(new_item)
+                monkey[-1] += 1
+                if new_item % test == 0:
+                    monkeys[true_monkey][0].append(new_item)
                 else:
-                    monkeys[monkey.false_monkey].append(new_item)
+                    monkeys[false_monkey][0].append(new_item)
 
-    monkey_active_counts = []
-    for monkey in monkeys:
-        monkey_active_counts.append(monkey.inspected_items)
+    monkey_active_counts = [monkey[-1] for monkey in monkeys]
     monkey_active_counts.sort(reverse=True)
 
-    print("Part 2: " + str(monkey_active_counts[0] * monkey_active_counts[1]))
+    print('Part 2:', monkey_active_counts[0] * monkey_active_counts[1])
