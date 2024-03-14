1655d4f8c2d40ca9b348a2a21d77dc5265b74568
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-03-14 15:11:31 +0100

    update 2020 day 3 part 2

---
+++ 2020/day_03/toboggan_trajectory_2.cpp
@@ -0,0 +1,56 @@
+// --- Day 2: Password Philosophy --- Part Two ---
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int countTrees(const vector<string> &treeMap, int right, int down) {
+    int treeCount = 0;
+    int index = 0;
+    for (int y = down; y < treeMap.size(); y += down) {
+        const string &line = treeMap[y];
+        index = (index + right) % (int) line.size();
+
+        if (line[index] == '#') {
+            treeCount++;
+        }
+    }
+
+    return treeCount;
+}
+
+int main() {
+    vector<string> treeMap;
+    string line;
+
+    ifstream fileReader("2020/day_03/toboggan_trajectory_input");
+
+    while (getline(fileReader, line)) {
+        treeMap.push_back(line);
+    }
+
+    int slopes[5][2] = {
+            {1, 1},
+            {3, 1},
+            {5, 1},
+            {7, 1},
+            {1, 2},
+    };
+
+    int treeCounts[5];
+    for (int i = 0; i < 5; i++) {
+        treeCounts[i] = countTrees(treeMap, slopes[i][0], slopes[i][1]);
+    }
+
+    unsigned int treeProduct = 1;
+    for (int treeCount: treeCounts) {
+        treeProduct *= treeCount;
+    }
+
+    cout << "Part 2: " << treeProduct << "\n";
+
+    fileReader.close();
+}
