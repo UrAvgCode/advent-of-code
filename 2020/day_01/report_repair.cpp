4e2d78665b184d7101ac58b8b721df492cf19409
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-18 20:43:35 +0200

    first cmake setup

---
+++ 2020/day_01/report_repair.cpp
@@ -0,0 +1,63 @@
+// --- Day 1: Report Repair --- Part One ---
+
+#include "benchmark.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <set>
+
+std::set<int> parse_file(const std::string &filename) {
+    std::set<int> entries;
+
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("unable to open file: " + filename);
+    }
+
+    std::string line;
+    while (getline(file, line)) {
+        entries.insert(std::stoi(line));
+    }
+
+    return entries;
+}
+
+int part_one(std::set<int> entries) {
+    for (auto entry: entries) {
+        int missing = 2020 - entry;
+        if (entries.find(missing) != entries.end()) {
+            return entry * missing;
+        }
+    }
+
+    return -1;
+}
+
+int part_two(std::set<int> entries) {
+    for (auto first = entries.begin(); first != entries.end(); first++) {
+        for (auto second = std::next(first); second != entries.end(); second++) {
+            int missing = 2020 - *first - *second;
+            if (entries.find(missing) != entries.end()) {
+                return *first * *second * missing;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    std::string filename = "../../input/2020/day_01/input.txt";
+    auto entries = parse_file(filename);
+
+    std::cout << "--- Day 1: Report Repair ---" << std::endl;
+
+    auto start = benchmark::start();
+    std::cout << "\nPart 1: " << part_one(entries) << std::endl;
+    benchmark::end(start);
+
+    start = benchmark::start();
+    std::cout << "\nPart 2: " << part_two(entries) << std::endl;
+    benchmark::end(start);
+}
---
+++ include/benchmark.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+namespace benchmark {
+    std::chrono::time_point<std::chrono::system_clock> start() {
+        return std::chrono::high_resolution_clock::now();
+    }
+
+    void end(std::chrono::time_point<std::chrono::system_clock> start) {
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        auto output = static_cast<double>(duration.count());
+
+        std::string unit;
+        if (output >= 1'000'000) {
+            output /= 1'000'000'000;
+            unit = "s";
+        } else if (output >= 1'000) {
+            output /= 1'000'000;
+            unit = "ms";
+        } else {
+            output /= 1'000;
+            unit = "us";
+        }
+
+        std::cout << "Time: " << output << unit << std::endl;
+    }
+}
