ff3b37e7a4b972e904c28e39eaf89a628162a5f2
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-19 11:44:48 +0200

    add 2020 part 4 to cmake project

--- 2020/day_04/passport_processing.cpp
+++ 2020/day_04/passport_processing.cpp
@@ -1,84 +1,114 @@
 // --- Day 4: Passport Processing ---
 
-#include <iostream>
+#include "benchmark.h"
+
+#include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <sstream>
 #include <unordered_map>
 #include <unordered_set>
 
-using namespace std;
+std::vector<std::unordered_map<std::string, std::string>> parse_file(const std::string &filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("unable to open file: " + filename);
+    }
 
-bool validatePassport(const unordered_map<string, string> &passport) {
-    int birthYear = stoi(passport.at("byr"));
-    if (birthYear < 1920 || birthYear > 2002) return false;
+    std::unordered_map<std::string, std::string> passport;
+    std::vector<std::unordered_map<std::string, std::string>> passports;
+    for (std::string line; getline(file, line);) {
+        if (line.empty()) {
+            passports.push_back(passport);
+            passport = {};
+        } else {
+            std::stringstream stream(line);
+            std::string token;
 
-    int issueYear = stoi(passport.at("iyr"));
-    if (issueYear < 2010 || issueYear > 2020) return false;
+            while (stream >> token) {
+                auto colon_pos = token.find(':');
+                if (colon_pos != std::string::npos) {
+                    auto field = token.substr(0, colon_pos);
+                    auto value = token.substr(colon_pos + 1);
 
-    int expirationYear = stoi(passport.at("eyr"));
-    if (expirationYear < 2020 || expirationYear > 2030) return false;
+                    if (field != "cid") passport[field] = value;
+                }
+            }
+        }
+    }
 
-    string hgt = passport.at("hgt");
-    string unit = hgt.substr(hgt.size() - 2, 2);
+    return passports;
+}
+
+bool validate_passport(const std::unordered_map<std::string, std::string> &passport) {
+    int birth_year = stoi(passport.at("byr"));
+    if (birth_year < 1920 || birth_year > 2002) return false;
+
+    int issue_year = stoi(passport.at("iyr"));
+    if (issue_year < 2010 || issue_year > 2020) return false;
+
+    int expiration_year = stoi(passport.at("eyr"));
+    if (expiration_year < 2020 || expiration_year > 2030) return false;
+
+    auto hgt = passport.at("hgt");
+    auto unit = hgt.substr(hgt.size() - 2, 2);
     int height = stoi(hgt.substr(0, hgt.size() - 2));
 
     if (unit == "cm" && (height < 150 || height > 193)) return false;
     if (unit == "in" && (height < 59 || height > 76)) return false;
     if (unit != "in" && unit != "cm") return false;
 
-    string hairColor = passport.at("hcl");
-    if (hairColor[0] != '#') return false;
-    for (unsigned char c: hairColor.substr(1, hairColor.size() - 1)) {
-        int ascii = int(c);
-        if ((ascii < 48 || ascii > 57) && (ascii < 97 || ascii > 102)) return false;
+    auto hair_color = passport.at("hcl");
+    if (hair_color[0] != '#') return false;
+    for (char c: hair_color.substr(1, hair_color.size() - 1)) {
+        if ((c < '0' || c > '9') && (c < 'a' || c > 'f')) return false;
     }
 
-    unordered_set<string> possibleColors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
-    string eyeColor = passport.at("ecl");
-    if (possibleColors.find(eyeColor) == possibleColors.end()) return false;
+    const std::unordered_set<std::string> possible_colors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+    std::string eye_color = passport.at("ecl");
+    if (possible_colors.find(eye_color) == possible_colors.end()) return false;
 
-    string passportID = passport.at("pid");
-    if (passportID.size() != 9) return false;
-    for (char c: passportID) {
-        if (!isdigit(c)) return false;
-    }
+    std::string passport_id = passport.at("pid");
+    if (passport_id.size() != 9) return false;
+    if (!std::ranges::all_of(passport_id, ::isdigit)) return false;
 
     return true;
 }
 
-int main() {
-    int validPassportCount = 0;
-    int strictValidPassportCount = 0;
-
-    ifstream fileReader("input/2020/day_04/input.txt");
-
-    string line;
-    unordered_map<string, string> passport;
-    while (getline(fileReader, line)) {
-        if (line.empty()) {
-            if (passport.size() == 7) {
-                validPassportCount++;
-                if(validatePassport(passport)) strictValidPassportCount++;
-            }
-            passport.clear();
-        } else {
-            line += " ";
-            while (!line.empty()) {
-                int colonIndex = (int) line.find(':');
-                int spaceIndex = (int) line.find(' ');
-
-                string field = line.substr(0, colonIndex);
-                string value = line.substr(colonIndex + 1, spaceIndex - (colonIndex + 1));
-
-                if (field != "cid") passport[field] = value;
-
-                line = line.substr(spaceIndex + 1, line.size() - (spaceIndex + 1));
-            }
+int part_one(const std::vector<std::unordered_map<std::string, std::string>> &passports) {
+    int valid_passport_count = 0;
+    for (const auto &passport: passports) {
+        if (passport.size() == 7) {
+            valid_passport_count++;
         }
     }
 
-    fileReader.close();
+    return valid_passport_count;
+}
 
-    cout << "Part 1: " << validPassportCount << "\n";
-    cout << "Part 2: " << strictValidPassportCount << "\n";
+int part_two(const std::vector<std::unordered_map<std::string, std::string>> &passports) {
+    int valid_passport_count = 0;
+    for (const auto &passport: passports) {
+        if (passport.size() == 7 && validate_passport(passport)) {
+            valid_passport_count++;
+        }
+    }
+
+    return valid_passport_count;
+}
+
+int main() {
+    std::string filename = "../../input/2020/day_04/input.txt";
+    auto passports = parse_file(filename);
+
+    std::cout << "--- Day 4: Passport Processing ---" << std::endl;
+
+    auto start = benchmark::start();
+    std::cout << "\nPart 1: " << part_one(passports) << std::endl;
+    benchmark::end(start);
+
+    start = benchmark::start();
+    std::cout << "\nPart 2: " << part_two(passports) << std::endl;
+    benchmark::end(start);
 }
