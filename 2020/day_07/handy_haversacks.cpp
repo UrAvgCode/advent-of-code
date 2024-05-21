987b83b770aa46300105a4f82c88d8274516a67b
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-21 10:56:22 +0200

    add 2020 day 7 part 2

--- 2020/day_07/handy_haversacks.cpp
+++ 2020/day_07/handy_haversacks.cpp
@@ -8,16 +8,15 @@
 #include <sstream>
 #include <queue>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
-std::unordered_map<std::string, std::unordered_set<std::string>> parse_file(const std::string &filename) {
+std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> parse_file(const std::string &filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         throw std::runtime_error("unable to open file: " + filename);
     }
 
-    std::unordered_map<std::string, std::unordered_set<std::string>> bags;
+    std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> bags;
     for (std::string line; getline(file, line);) {
         std::vector<std::string> tokens;
 
@@ -29,14 +28,15 @@ std::unordered_map<std::string, std::unordered_set<std::string>> parse_file(cons
         }
 
         auto key = tokens[0] + " " + tokens[1];
-        auto contains = std::unordered_set<std::string>();
-        for (int i = 5; i < tokens.size(); i += 4) {
-            if (tokens[i] == "other") {
+        std::vector<std::pair<int, std::string>> contains;
+        for (int i = 4; i < tokens.size(); i += 4) {
+            if (tokens[i] == "no") {
                 break;
             }
 
-            auto bag = tokens[i] + " " + tokens[i + 1];
-            contains.insert(bag);
+            int amount = std::stoi(tokens[i]);
+            auto bag = tokens[i + 1] + " " + tokens[i + 2];
+            contains.emplace_back(amount, bag);
         }
 
         bags[key] = contains;
@@ -45,13 +45,14 @@ std::unordered_map<std::string, std::unordered_set<std::string>> parse_file(cons
     return bags;
 }
 
-int contains_shiny_gold(const std::unordered_map<std::string, std::unordered_set<std::string>> &bags_map, const std::string &bag) {
+int contains_shiny_gold(const std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> &bags_map,
+                        const std::string &bag) {
     auto bag_queue = std::queue<std::string>({bag});
     while (!bag_queue.empty()) {
         auto current_bag = bag_queue.front();
         bag_queue.pop();
 
-        for(const auto &containing_bag : bags_map.at(current_bag)) {
+        for (const auto &[amount, containing_bag]: bags_map.at(current_bag)) {
             if (containing_bag == "shiny gold") {
                 return 1;
             } else {
@@ -64,13 +65,34 @@ int contains_shiny_gold(const std::unordered_map<std::string, std::unordered_set
     return 0;
 }
 
-int part_one(const std::unordered_map<std::string, std::unordered_set<std::string>> &bags_map) {
+int part_one(const std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> &bags_map) {
     int count_of_shiny_gold_bags = 0;
     for (const auto &[initial_bag, contains]: bags_map) {
         count_of_shiny_gold_bags += contains_shiny_gold(bags_map, initial_bag);
     }
 
     return count_of_shiny_gold_bags;
+}
+
+int part_two(const std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> &bags_map) {
+    int count_of_bags = 0;
+
+    auto bag_queue = std::queue<std::pair<int, std::string>>();
+    bag_queue.emplace(1, "shiny gold");
+
+    while (!bag_queue.empty()) {
+        auto [current_amount, current_bag] = bag_queue.front();
+        bag_queue.pop();
+
+        count_of_bags += current_amount;
+
+        const auto &contents = bags_map.at(current_bag);
+        for (const auto &[containing_amount, containing_bag]: contents) {
+            bag_queue.emplace(containing_amount * current_amount, containing_bag);
+        }
+    }
+
+    return count_of_bags - 1;
 }
 
 int main() {
@@ -81,6 +103,10 @@ int main() {
 
     auto start = benchmark::start();
     std::cout << "\nPart 1: " << part_one(bags) << std::endl;
+    benchmark::end(start);
+
+    start = benchmark::start();
+    std::cout << "\nPart 2: " << part_two(bags) << std::endl;
     benchmark::end(start);
 
     return 0;
