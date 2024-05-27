95f8b0db644e22e16d158c8f7fcfe42a5fbbc30a
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-27 09:37:56 +0200

    add 2020 day 12 part 2

--- 2020/day_12/rain_risk.cpp
+++ 2020/day_12/rain_risk.cpp
@@ -24,7 +24,7 @@ std::vector<std::pair<char, int>> parse_file(const std::string &filename) {
     return navigation_instructions;
 }
 
-double part_one(const std::vector<std::pair<char, int>> &navigation_instructions) {
+int part_one(const std::vector<std::pair<char, int>> &navigation_instructions) {
     int x = 0;
     int y = 0;
     int direction = 0;
@@ -61,6 +61,60 @@ double part_one(const std::vector<std::pair<char, int>> &navigation_instructions
     return std::abs(x) + std::abs(y);
 }
 
+int part_two(const std::vector<std::pair<char, int>> &navigation_instructions) {
+    int waypoint_x = 10;
+    int waypoint_y = 1;
+    int x = 0;
+    int y = 0;
+
+    for (const auto &[action, amount]: navigation_instructions) {
+        switch (action) {
+            case 'N':
+                waypoint_y += amount;
+                break;
+            case 'S':
+                waypoint_y -= amount;
+                break;
+            case 'E':
+                waypoint_x += amount;
+                break;
+            case 'W':
+                waypoint_x -= amount;
+                break;
+            case 'L': {
+                int sin = static_cast<int>(std::sin(amount * (M_PI / 180.0)));
+                int cos = static_cast<int>(std::cos(amount * (M_PI / 180.0)));
+
+                int next_x = waypoint_x * cos + waypoint_y * -sin;
+                int next_y = waypoint_x * sin + waypoint_y * cos;
+
+                waypoint_x = next_x;
+                waypoint_y = next_y;
+                break;
+            }
+            case 'R': {
+                int sin = static_cast<int>(std::sin(amount * (M_PI / 180.0)));
+                int cos = static_cast<int>(std::cos(amount * (M_PI / 180.0)));
+
+                int next_x = waypoint_x * cos + waypoint_y * sin;
+                int next_y = waypoint_x * -sin + waypoint_y * cos;
+
+                waypoint_x = next_x;
+                waypoint_y = next_y;
+                break;
+            }
+            case 'F':
+                x += waypoint_x * amount;
+                y += waypoint_y * amount;
+                break;
+            default:
+                std::cerr << "invalid action: " << action << std::endl;
+        }
+    }
+
+    return std::abs(x) + std::abs(y);
+}
+
 int main() {
     std::string filename = "../../input/2020/day_12/input.txt";
     auto navigation_instructions = parse_file(filename);
@@ -69,6 +123,10 @@ int main() {
 
     auto start = benchmark::start();
     std::cout << "\nPart 1: " << part_one(navigation_instructions) << std::endl;
+    benchmark::end(start);
+
+    start = benchmark::start();
+    std::cout << "\nPart 2: " << part_two(navigation_instructions) << std::endl;
     benchmark::end(start);
 
     return 0;
