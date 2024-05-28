4a5429d27933e36ffd0f131168850134eda7cef3
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-28 15:09:24 +0200

    update 2020 day 13

--- 2020/day_13/shuttle_search.cpp
+++ 2020/day_13/shuttle_search.cpp
@@ -2,13 +2,12 @@
 
 #include "benchmark.h"
 
-#include <cmath>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
-#include <vector>
 
-std::pair<int, std::vector<int>> parse_file(const std::string &filename) {
+std::pair<int, std::map<int, int>> parse_file(const std::string &filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         throw std::runtime_error("unable to open file: " + filename);
@@ -20,42 +19,38 @@ std::pair<int, std::vector<int>> parse_file(const std::string &filename) {
 
     getline(file, line);
     std::stringstream stream(line);
-    std::vector<int> bus_ids;
+
+    int number = 0;
+    std::map<int, int> buses;
     for (std::string token; getline(stream, token, ',');) {
-        if (token == "x") {
-            bus_ids.push_back(-1);
-        } else {
-            bus_ids.push_back(std::stoi(token));
+        if (token != "x") {
+            buses[number] = std::stoi(token);
         }
+        number++;
     }
 
-    return {earliest_timestamp, bus_ids};
+    return {earliest_timestamp, buses};
 }
 
-int part_one(const int earliest_timestamp, const std::vector<int> &bus_ids) {
-    auto wait_times = std::vector<int>(bus_ids.size(), std::numeric_limits<int>::max());
-    for (int i = 0; i < bus_ids.size(); i++) {
-        auto id = bus_ids[i];
-        if (id == -1) continue;
-        wait_times[i] = id - (earliest_timestamp % id);
+int part_one(const int earliest_timestamp, const std::map<int, int> &buses) {
+    auto wait_times = std::map<int, int>();
+    for (const auto &[_, bus_id]: buses) {
+        wait_times[bus_id - (earliest_timestamp % bus_id)] = bus_id;
     }
 
-    auto min_element = std::min_element(wait_times.begin(), wait_times.end());
-    auto index = std::distance(wait_times.begin(), min_element);
-
-    return bus_ids[index] * wait_times[index];
+    auto &[wait_time, bus_id] = *std::min_element(wait_times.begin(), wait_times.end());
+    return wait_time * bus_id;
 }
 
-std::uint64_t part_two(const std::vector<int> &bus_ids) {
-    std::uint64_t earliest_timestamp = 0;
-    auto step_size = static_cast<std::uint64_t>(bus_ids[0]);
+std::uint64_t part_two(const std::map<int, int> &buses) {
+    std::uint64_t earliest_timestamp = buses.at(0);
+    std::uint64_t step_size = 1;
 
-    for(int i = 1; i < bus_ids.size(); i++) {
-        if(bus_ids[i] == -1) continue;
-        while ((earliest_timestamp + i) % bus_ids[i] != 0) {
+    for (const auto &[number, id]: buses) {
+        while ((earliest_timestamp + number) % id != 0) {
             earliest_timestamp += step_size;
         }
-        step_size *= bus_ids[i];
+        step_size *= id;
     }
 
     return earliest_timestamp;
@@ -63,16 +58,16 @@ std::uint64_t part_two(const std::vector<int> &bus_ids) {
 
 int main() {
     std::string filename = "../../input/2020/day_13/input.txt";
-    auto [earliest_timestamp, bus_ids] = parse_file(filename);
+    auto [earliest_timestamp, buses] = parse_file(filename);
 
     std::cout << "--- Day 13: Shuttle Search ---" << std::endl;
 
     auto start = benchmark::start();
-    std::cout << "\nPart 1: " << part_one(earliest_timestamp, bus_ids) << std::endl;
+    std::cout << "\nPart 1: " << part_one(earliest_timestamp, buses) << std::endl;
     benchmark::end(start);
 
     start = benchmark::start();
-    std::cout << "\nPart 2: " << part_two(bus_ids) << std::endl;
+    std::cout << "\nPart 2: " << part_two(buses) << std::endl;
     benchmark::end(start);
 
     return 0;
