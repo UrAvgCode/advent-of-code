648b9cd2dd4c0bf7d0a8545a0e00b0bb1676c0b0
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-29 14:39:45 +0200

    update 2020 day 14

--- 2020/day_14/docking_data.cpp
+++ 2020/day_14/docking_data.cpp
@@ -6,60 +6,62 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
-std::vector<std::pair<std::string, std::string>> parse_file(const std::string &filename) {
+using Memory = std::unordered_map<std::uint64_t, std::uint64_t>;
+using Program = std::vector<std::pair<std::string, Memory>>;
+
+Program parse_file(const std::string &filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         throw std::runtime_error("unable to open file: " + filename);
     }
 
-    std::vector<std::pair<std::string, std::string>> initialization_program;
-    for(std::string line; std::getline(file, line);) {
-        auto equals_index = line.find('=');
-        std::string action = line.substr(0, equals_index - 1);
-        std::string value = line.substr(equals_index + 2);
-
-        if(action != "mask") {
-            action = action.substr(4, action.length() - 1);
+    std::string mask;
+    Memory memory;
+    Program initialization_program;
+    for (std::string line; std::getline(file, line);) {
+        if (line.substr(0, 4) == "mask") {
+            if (!memory.empty()) {
+                initialization_program.emplace_back(mask, memory);
+                memory = Memory();
+            }
+            mask = line.substr(7);
+        } else {
+            auto equals_index = line.find('=');
+            auto address = std::stoull(line.substr(4, equals_index - 6));
+            auto value = std::stoull(line.substr(equals_index + 2));
+            memory[address] = value;
         }
-
-        initialization_program.emplace_back(action, value);
     }
+    initialization_program.emplace_back(mask, memory);
 
     return initialization_program;
 }
 
-std::uint64_t part_one(const std::vector<std::pair<std::string, std::string>>& initialization_program) {
-    std::uint64_t and_mask = std::numeric_limits<std::uint64_t>::max();
-    std::uint64_t or_mask = 0;
+std::uint64_t part_one(const Program &initialization_program) {
+    Memory computer_memory;
+    for (auto &[mask, memory]: initialization_program) {
+        std::uint64_t and_mask = std::numeric_limits<std::uint64_t>::max();
+        std::uint64_t or_mask = 0;
+        std::uint64_t bit_mask = 1ull << 36;
 
-    std::unordered_map<std::uint64_t, std::uint64_t> memory;
-    for(const auto &[action, value]: initialization_program) {
-        if(action == "mask") {
-            and_mask = std::numeric_limits<std::uint64_t>::max();
-            or_mask = 0;
-
-            for(std::uint64_t i = 0; i < value.size(); i++) {
-                char current_bit = value[value.size() - 1 - i];
-                if(current_bit == '1') {
-                    or_mask = or_mask | (1ull << i);
-                } else if(current_bit == '0') {
-                    and_mask = and_mask & ~(1ull << i);
-                }
+        for (auto bit: mask) {
+            bit_mask >>= 1;
+            if (bit == '1') {
+                or_mask |= bit_mask;
+            } else if (bit == '0') {
+                and_mask &= ~bit_mask;
             }
-        } else {
-            std::uint64_t memory_address = std::stoull(action);
-            std::uint64_t memory_value = std::stoull(value);
+        }
 
-            memory_value = memory_value & and_mask;
-            memory_value = memory_value | or_mask;
-
-            memory[memory_address] = memory_value;
+        for (auto &[address, value]: memory) {
+            computer_memory[address] = value & and_mask | or_mask;
         }
     }
 
     std::uint64_t sum_of_all_values = 0;
-    for(auto &[_, value]: memory) {
+    for (auto &[_, value]: computer_memory) {
         sum_of_all_values += value;
     }
 
