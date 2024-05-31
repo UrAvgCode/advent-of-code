352344bf88aa75b1a69b3e2a568e3edb25e87d83
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-05-31 18:50:54 +0200

    add 2020 day 15 part 2

--- 2020/day_15/rambunctious_recitation.cpp
+++ 2020/day_15/rambunctious_recitation.cpp
@@ -8,7 +8,7 @@
 #include <unordered_map>
 #include <vector>
 
-std::vector<int> parse_file(const std::string &filename) {
+std::vector<std::uint64_t> parse_file(const std::string &filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         throw std::runtime_error("unable to open file: " + filename);
@@ -17,29 +17,25 @@ std::vector<int> parse_file(const std::string &filename) {
     std::stringstream stream;
     stream << file.rdbuf();
 
-    std::vector<int> starting_numbers;
+    std::vector<std::uint64_t> starting_numbers;
     for (std::string token; getline(stream, token, ',');) {
-        starting_numbers.push_back(std::stoi(token));
+        starting_numbers.push_back(std::stoull(token));
     }
 
     return starting_numbers;
 }
 
-std::uint64_t part_one(const std::vector<int> &starting_numbers) {
-    std::unordered_map<int, int> spoken_numbers;
+std::uint64_t get_nth_number(const std::vector<std::uint64_t> &starting_numbers, std::size_t n) {
+    std::unordered_map<std::uint64_t, std::size_t> spoken_numbers;
+    for (auto i = 0; i < starting_numbers.size() - 1; i++) {
+        spoken_numbers[starting_numbers[i]] = i + 1;
+    }
 
-    int prev_number = starting_numbers.back();
-    for (int i = 1; i < 2020; i++) {
-        if (i < starting_numbers.size()) {
-            spoken_numbers[starting_numbers[i - 1]] = i;
-        } else if (spoken_numbers.contains(prev_number)) {
-            int difference = i - spoken_numbers[prev_number];
-            spoken_numbers[prev_number] = i;
-            prev_number = difference;
-        } else {
-            spoken_numbers[prev_number] = i;
-            prev_number = 0;
-        }
+    auto prev_number = starting_numbers.back();
+    for (auto i = starting_numbers.size(); i < n; i++) {
+        auto next_number = spoken_numbers.contains(prev_number) ? i - spoken_numbers[prev_number] : 0;
+        spoken_numbers[prev_number] = i;
+        prev_number = next_number;
     }
 
     return prev_number;
@@ -52,7 +48,11 @@ int main() {
     std::cout << "--- Day 15: Rambunctious Recitation ---" << std::endl;
 
     auto start = benchmark::start();
-    std::cout << "\nPart 1: " << part_one(starting_numbers) << std::endl;
+    std::cout << "\nPart 1: " << get_nth_number(starting_numbers, 2020) << std::endl;
+    benchmark::end(start);
+
+    start = benchmark::start();
+    std::cout << "\nPart 2: " << get_nth_number(starting_numbers, 30000000) << std::endl;
     benchmark::end(start);
 
     return 0;
