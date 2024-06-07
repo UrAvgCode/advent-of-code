56bd704dd083a067ccb50d5a6183f4c178e5d0f0
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-06-07 16:48:57 +0200

    modernize 2020 day 2

--- 2020/day_02/password_philosophy.cpp
+++ 2020/day_02/password_philosophy.cpp
@@ -5,67 +5,61 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <tuple>
 #include <vector>
 
-using PasswordList = std::vector<std::tuple<std::string, char, int, int>>;
+using PasswordPolicy = std::tuple<int, int, char, std::string>;
 
-PasswordList parse_file(const std::string &filename) {
+std::vector<PasswordPolicy> parse_file(const std::string &filename) {
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (file.fail()) {
         throw std::runtime_error("unable to open file: " + filename);
     }
 
-    PasswordList passwords;
+    std::vector<PasswordPolicy> passwords;
     for (std::string line; getline(file, line);) {
-        auto hyphen_index = line.find('-');
-        auto space_index = line.find(' ');
+        std::stringstream stream(line);
 
-        int lowest = stoi(line.substr(0, hyphen_index));
-        int highest = stoi(line.substr(hyphen_index + 1, space_index - hyphen_index));
+        int min, max;
+        char character;
+        std::string password;
+        char dummy;
 
-        char character = line[space_index + 1];
-        auto password = line.substr(space_index + 4);
-
-        passwords.emplace_back(password, character, lowest, highest);
+        stream >> min >> dummy >> max >> character >> dummy >> password;
+        passwords.emplace_back(min, max, character, password);
     }
 
     return passwords;
 }
 
-int part_one(const PasswordList &passwords) {
-    int valid_password_count = 0;
-    for (const auto &[password, character, lowest, highest]: passwords) {
-        auto char_count = static_cast<int>(std::ranges::count(password.begin(), password.end(), character));
-        if (char_count >= lowest && char_count <= highest) {
-            valid_password_count++;
-        }
-    }
-    return valid_password_count;
+std::size_t part_one(const std::vector<PasswordPolicy> &password_policies) {
+    return std::ranges::count_if(password_policies, [](const auto &policy) {
+        const auto &[min, max, character, password] = policy;
+        auto char_count = std::ranges::count(password, character);
+        return char_count >= min && char_count <= max;
+    });
 }
 
-int part_two(const PasswordList &passwords) {
-    int valid_password_count = 0;
-    for (const auto &[password, character, pos_one, pos_two]: passwords) {
-        if ((password[pos_one - 1] == character) != (password[pos_two - 1] == character)) {
-            valid_password_count++;
-        }
-    }
-    return valid_password_count;
+std::size_t part_two(const std::vector<PasswordPolicy> &password_policies) {
+    return std::ranges::count_if(password_policies, [](const auto &policy) {
+        const auto &[pos_one, pos_two, character, password] = policy;
+        return (password[pos_one - 1] == character) != (password[pos_two - 1] == character);
+    });
 }
 
 int main() {
     std::string filename = "../../input/2020/day_02/input.txt";
-    auto passwords = parse_file(filename);
+    auto password_policies = parse_file(filename);
 
     std::cout << "--- Day 2: Password Philosophy ---" << std::endl;
 
     auto start = benchmark::start();
-    std::cout << "\nPart 1: " << part_one(passwords) << std::endl;
+    std::cout << "\nPart 1: " << part_one(password_policies) << std::endl;
     benchmark::end(start);
 
     start = benchmark::start();
-    std::cout << "\nPart 1: " << part_two(passwords) << std::endl;
+    std::cout << "\nPart 2: " << part_two(password_policies) << std::endl;
     benchmark::end(start);
 }
