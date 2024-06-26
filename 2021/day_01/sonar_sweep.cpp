42ea84c4132e26e88bac1831f3c3292f148028ce
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-06-26 18:13:36 +0200

    update 2021 day 1 cpp

--- 2021/day_01/sonar_sweep.cpp
+++ 2021/day_01/sonar_sweep.cpp
@@ -2,36 +2,22 @@
 
 #include "solver.h"
 
-#include <algorithm>
 #include <fstream>
+#include <iterator>
+#include <numeric>
 #include <string>
 #include <vector>
 
 std::vector<int> parser(std::ifstream &file) {
-    std::vector<int> depths;
-    for (std::string line; getline(file, line);) {
-        depths.push_back(std::stoi(line));
-    }
-
-    return depths;
+    return {std::istream_iterator<int>(file), std::istream_iterator<int>()};
 }
 
 int part_one(const std::vector<int> &depths) {
-    int count = 0;
-    for (std::size_t i = 0; i < depths.size() - 1; ++i) {
-        count += depths[i] < depths[i + 1];
-    }
-
-    return count;
+    return std::inner_product(depths.begin(), depths.end() - 1, depths.begin() + 1, 0, std::plus<>(), std::less<>());
 }
 
 int part_two(const std::vector<int> &depths) {
-    int count = 0;
-    for (std::size_t i = 0; i < depths.size() - 3; ++i) {
-        count += depths[i] < depths[i + 3];
-    }
-
-    return count;
+    return std::inner_product(depths.begin(), depths.end() - 3, depths.begin() + 3, 0, std::plus<>(), std::less<>());
 }
 
 int main() {
