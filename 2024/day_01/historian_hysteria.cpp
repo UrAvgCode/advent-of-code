0ee093d1a5b16ed315614622d61ecd63ac48698f
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-12-25 16:28:05 +0100

    add 2024 day 1 part 1

---
+++ 2024/day_01/historian_hysteria.cpp
@@ -0,0 +1,44 @@
+// --- Day 1: Historian Hysteria ---
+
+#include "solver.h"
+
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <numeric>
+#include <string>
+
+std::array<std::vector<int>, 2> parser(std::ifstream &file) {
+    std::vector<int> left_list;
+    std::vector<int> right_list;
+
+    for (std::string line; getline(file, line);) {
+        left_list.push_back(std::stoi(line.substr(0, 5)));
+        right_list.push_back(std::stoi(line.substr(8, 5)));
+    }
+
+    return {left_list, right_list};
+}
+
+int part_one(const std::vector<int> &left_list, const std::vector<int> &right_list) {
+    auto left_list_sorted = left_list;
+    auto right_list_sorted = right_list;
+
+    std::ranges::sort(left_list_sorted);
+    std::ranges::sort(right_list_sorted);
+
+    const int total_distance = std::transform_reduce(
+            left_list_sorted.begin(), left_list_sorted.end(), right_list_sorted.begin(), 0, std::plus<>(),
+            [](const int left_number, const int right_number) { return std::abs(left_number - right_number); });
+
+    return total_distance;
+}
+
+int main() {
+    Solver solver(2024, 1, "Historian Hysteria");
+
+    const auto [left_list, right_list] = solver.parse_file(parser);
+    solver(part_one, left_list, right_list);
+
+    return 0;
+}
