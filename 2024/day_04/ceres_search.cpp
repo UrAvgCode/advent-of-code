7c2878d405d1fbcae0d801f3c37cd5e31f621d12
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-12-26 03:11:11 +0100

    add 2024 day 4 part 2

--- 2024/day_04/ceres_search.cpp
+++ 2024/day_04/ceres_search.cpp
@@ -62,11 +62,33 @@ std::uint64_t part_one(const std::vector<std::string> &word_map) {
     return horizontal_count + vertical_count + diagonal_count;
 }
 
+std::uint64_t part_two(const std::vector<std::string> &word_map) {
+    std::uint64_t count = 0;
+    for (std::size_t y = 0; y < word_map.size() - 2; ++y) {
+        for (std::size_t x = 0; x < word_map[0].size() - 2; ++x) {
+            if (word_map[y + 1][x + 1] == 'A') {
+                const char top_left = word_map[y][x];
+                const char top_right = word_map[y][x + 2];
+                const char bottom_left = word_map[y + 2][x];
+                const char bottom_right = word_map[y + 2][x + 2];
+
+                if (((top_left == 'M' && bottom_right == 'S') || (top_left == 'S' && bottom_right == 'M')) &&
+                    ((top_right == 'M' && bottom_left == 'S') || (top_right == 'S' && bottom_left == 'M'))) {
+                    ++count;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
 int main() {
     Solver solver(2024, 4, "Ceres Search");
 
     const auto word_map = solver.parse_file(parser);
     solver(part_one, word_map);
+    solver(part_two, word_map);
 
     return 0;
 }
